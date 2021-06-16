ed7c399a3d62c4a70ef741083370acb44540d6a4
Roope Korkee <roope.korkee@tuni.fi>
2021-06-16 14:57:22 +0300

    Reformat code

--- src/main/java/me/roopekoo/distance/Distance.java
+++ src/main/java/me/roopekoo/distance/Distance.java
@@ -9,99 +9,91 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Distance extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        System.out.println("The Distance plugin started");
+	private static double FastSqrt(double number) {
+		double x = number;
+		double xhalf = 0.5d*x;
+		long i = Double.doubleToLongBits(x);
+		i = 0x5fe6ec85e7de30daL-(i >> 1);
+		x = Double.longBitsToDouble(i);
+		for(int it = 0; it<4; it++) {
+			x = x*(1.5d-xhalf*x*x);
+		}
+		x *= number;
+		return x;
+	}
 
-    }
+	@Override public void onEnable() {
+		System.out.println("The Distance plugin started");
 
-    @Override
-    public void onDisable() {
-        System.out.println("The Distance plugin has been shut down.");
-    }
+	}
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equals("distance")) {
-            if (sender instanceof Player) {
-                if (args.length == 1) {
-                    Player target1 = (Player) sender;
-                    Player target2 = Bukkit.getPlayerExact(args[0]);
-                    if (isTargetPlayer(target2, sender)) {
-                        assert target2 != null;
-                        calcDistance(target1, target2, sender);
-                    }
-                }
-                else if (args.length == 2) {
-                    Player target1 = Bukkit.getPlayerExact(args[0]);
-                    Player target2 = Bukkit.getPlayerExact(args[1]);
-                    if (isTargetPlayer(target1, sender) && isTargetPlayer(target2, sender)) {
-                        assert target1 != null;
-                        assert target2 != null;
-                        calcDistance(target1, target2, sender);
-                    }
-                }
-                else {
-                    sender.sendMessage("Incorrect amount of arguments! Expected 1 or 2, got " + args.length);
-                }
-            }
-            else {
-                if (args.length == 2) {
-                    Player target1 = Bukkit.getPlayerExact(args[0]);
-                    Player target2 = Bukkit.getPlayerExact(args[1]);
-                    if (isTargetPlayer(target1, sender) && isTargetPlayer(target2, sender)) {
-                        assert target1 != null;
-                        assert target2 != null;
-                        calcDistance(target1, target2, sender);
-                    }
-                }
-                else {
-                    System.out.println("Incorrect amount of arguments! Expected 2, got " + args.length);
-                }
-            }
-        }
-        return true;
-    }
+	@Override public void onDisable() {
+		System.out.println("The Distance plugin has been shut down.");
+	}
 
-    private boolean isTargetPlayer(Player target, CommandSender sender) {
-        if (target != null) {
-            return true;
-        }
-        else {
-            String message = "That player doesn't exist!";
-            sender.sendMessage(message);
-            return false;
-        }
-    }
+	@Override public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(command.getName().equals("distance")) {
+			if(sender instanceof Player) {
+				if(args.length == 1) {
+					Player target1 = (Player) sender;
+					Player target2 = Bukkit.getPlayerExact(args[0]);
+					if(isTargetPlayer(target2, sender)) {
+						assert target2 != null;
+						calcDistance(target1, target2, sender);
+					}
+				} else if(args.length == 2) {
+					Player target1 = Bukkit.getPlayerExact(args[0]);
+					Player target2 = Bukkit.getPlayerExact(args[1]);
+					if(isTargetPlayer(target1, sender) && isTargetPlayer(target2, sender)) {
+						assert target1 != null;
+						assert target2 != null;
+						calcDistance(target1, target2, sender);
+					}
+				} else {
+					sender.sendMessage("Incorrect amount of arguments! Expected 1 or 2, got "+args.length);
+				}
+			} else {
+				if(args.length == 2) {
+					Player target1 = Bukkit.getPlayerExact(args[0]);
+					Player target2 = Bukkit.getPlayerExact(args[1]);
+					if(isTargetPlayer(target1, sender) && isTargetPlayer(target2, sender)) {
+						assert target1 != null;
+						assert target2 != null;
+						calcDistance(target1, target2, sender);
+					}
+				} else {
+					System.out.println("Incorrect amount of arguments! Expected 2, got "+args.length);
+				}
+			}
+		}
+		return true;
+	}
 
-    private void calcDistance(Player target1, Player target2, CommandSender sender) {
-        Location loc1 = target1.getLocation();
-        Location loc2 = target2.getLocation();
-        if (loc1.getWorld() != loc2.getWorld()) {
-            String message = "Players are not in the same world!";
-            sender.sendMessage(message);
-        }
-        else {
-            double x = loc1.getX()-loc2.getX();
-            double y = loc1.getY()-loc2.getY();
-            double z = loc1.getZ()-loc2.getZ();
-            double distSqrt = x*x+y*y+z*z;
-            double distance = FastSqrt(distSqrt);
-            String message = "The distance between "+target1.getName()+" and "+target2.getName()+" is "+String.format("%.2f", distance)+" meters.";
-            sender.sendMessage(message);
-        }
-    }
+	private boolean isTargetPlayer(Player target, CommandSender sender) {
+		if(target != null) {
+			return true;
+		} else {
+			String message = "That player doesn't exist!";
+			sender.sendMessage(message);
+			return false;
+		}
+	}
 
-    private static double FastSqrt(double number) {
-        double x = number;
-        double xhalf = 0.5d*x;
-        long i = Double.doubleToLongBits(x);
-        i = 0x5fe6ec85e7de30daL-(i >> 1);
-        x = Double.longBitsToDouble(i);
-        for(int it = 0; it<4; it++) {
-            x = x*(1.5d-xhalf*x*x);
-        }
-        x *= number;
-        return x;
-    }
+	private void calcDistance(Player target1, Player target2, CommandSender sender) {
+		Location loc1 = target1.getLocation();
+		Location loc2 = target2.getLocation();
+		if(loc1.getWorld() != loc2.getWorld()) {
+			String message = "Players are not in the same world!";
+			sender.sendMessage(message);
+		} else {
+			double x = loc1.getX()-loc2.getX();
+			double y = loc1.getY()-loc2.getY();
+			double z = loc1.getZ()-loc2.getZ();
+			double distSqrt = x*x+y*y+z*z;
+			double distance = FastSqrt(distSqrt);
+			String message = "The distance between "+target1.getName()+" and "+target2.getName()+" is "+
+			                 String.format("%.2f", distance)+" meters.";
+			sender.sendMessage(message);
+		}
+	}
 }
